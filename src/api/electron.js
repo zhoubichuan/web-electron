732a1ed7bfab49d04703fbaa8318458f690ea531
周必川 <zhoubichuan@icloud.com>
2025-11-26 23:22:03 +0800

    更新

--- src/api/electron.js
+++ src/api/electron.js
@@ -10,3 +10,12 @@ export const getDeviceInfo = async (name, value) => {
 export const getMac = async () => await getDeviceInfo('getMac', '')
 export const getIp = async () => await getDeviceInfo('getIp', '')
 export const getInfo = async () => await getDeviceInfo('getInfo', '')
+
+export const npm = async (value) => {
+  window.electron?.ipcRenderer?.sendMessage('npm', [value]);
+  return new Promise((resolve) => {
+    window.electron?.ipcRenderer?.once('npm', (arg) => {
+      resolve(arg);
+    });
+  });
+}
