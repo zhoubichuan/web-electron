fca558252463f3ea73d9020363d2e3e97f9c0bf8
周必川 <zhoubichuan@icloud.com>
2025-12-07 00:09:17 +0800

    更新

--- src/api/electron.js
+++ src/api/electron.js
@@ -1,16 +1,3 @@
-export const getDeviceInfo = async (name, value) => {
-  window.electron?.ipcRenderer?.sendMessage(name, [value]);
-  return new Promise((resolve) => {
-    window.electron?.ipcRenderer?.once(name, (arg) => {
-      resolve(arg);
-    });
-  });
-}
-
-export const getMac = async () => await getDeviceInfo('getMac', '')
-export const getIp = async () => await getDeviceInfo('getIp', '')
-export const getInfo = async () => await getDeviceInfo('getInfo', '')
-
 export const npm = async (value) => {
   window.electron?.ipcRenderer?.sendMessage('npm', [value]);
   return new Promise((resolve) => {
@@ -85,6 +72,14 @@ export const os = async (value) => {
   window.electron?.ipcRenderer?.sendMessage('os', [value]);
   return new Promise((resolve) => {
     window.electron?.ipcRenderer?.once('os', (arg) => {
+      resolve(arg);
+    });
+  });
+}
+export const file = async (value) => {
+  window.electron?.ipcRenderer?.sendMessage('file', [value]);
+  return new Promise((resolve) => {
+    window.electron?.ipcRenderer?.once('file', (arg) => {
       resolve(arg);
     });
   });
